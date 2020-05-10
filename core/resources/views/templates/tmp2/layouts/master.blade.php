72a616ad9e3a598366f219b27220e18b1024f23a
Perekpo Paulinus <perepau15@gmail.com>
2020-05-10 20:41:07 +0100

    created back linking from dashboard to frontend

--- core/resources/views/templates/tmp2/layouts/master.blade.php
+++ core/resources/views/templates/tmp2/layouts/master.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,7 @@
 
 
     <link rel="shortcut icon" href="{{get_image(config('constants.logoIcon.path') .'/favicon.png')}}"
-          type="image/x-icon">
+        type="image/x-icon">
 
     <title>{{ $general->sitename(__($page_title)) }} </title>
 
@@ -34,10 +35,10 @@
 
     @php echo $general->chat_script; @endphp
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" />
 
     <link rel="stylesheet"
-          href='{{ asset(activeTemplate(true) . "front/css/color.php?color=$general->bclr&color2=$general->sclr")}}'>
+        href='{{ asset(activeTemplate(true) . "front/css/color.php?color=$general->bclr&color2=$general->sclr")}}'>
 
 
 </head>
@@ -45,173 +46,173 @@
 
 <body>
 
-<div class="preloader">
-    <div class="preloader-inner">
-        <div class="preloader-icon">
-            <span></span>
-            <span></span>
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="overlay"></div>
-<a href="#0" class="scrollToTop">
-    <i class="fas fa-angle-up"></i>
-</a>
+    <div class="overlay"></div>
+    <a href="#0" class="scrollToTop">
+        <i class="fas fa-angle-up"></i>
+    </a>
 
-<header>
-    <div class="header-section">
-        <div class="container">
-            <div class="header-area">
-                <div class="logo">
-                    <a href="{{url('/')}}"><img src="{{get_image(config('constants.logoIcon.path') .'/logo.png')}}"
-                                                alt="logo"></a>
-                </div>
-                <ul class="menu">
+    <header>
+        <div class="header-section">
+            <div class="container">
+                <div class="header-area">
+                    <div class="logo">
+                        <a href="{{url('/')}}"><img src="{{get_image(config('constants.logoIcon.path') .'/logo.png')}}"
+                                alt="logo"></a>
+                    </div>
+                    <ul class="menu">
 
-                    <li>
-                        <a href="{{url('/')}}">@lang('Home')</a>
-                    </li>
-                     <li>
-                        <a href="{{url('/product')}}">@lang('Product')</a>
-                    </li>
+                        <li>
+                            <a href="{{url('/')}}">@lang('Home')</a>
+                        </li>
+                        <li>
+                            <a href="{{url('/product')}}">@lang('Product')</a>
+                        </li>
 
-                    <li><a @if(request()->path() == '/') href="#about"
-                           @else href="{{url('/')}}#about" @endif>@lang('About')</a></li>
+                        <li><a @if(request()->path() == '/') href="#about"
+                                @else href="{{url('/')}}#about" @endif>@lang('About')</a></li>
 
-                    <li><a @if(request()->path() == '/') href="#how-it-works"
-                           @else href="{{url('/')}}#how-it-works" @endif>@lang('How It Works')</a></li>
+                        <li><a @if(request()->path() == '/') href="#how-it-works"
+                                @else href="{{url('/')}}#how-it-works" @endif>@lang('How It Works')</a></li>
 
-                    <li><a @if(request()->path() == '/') href="#plan"
-                           @else href="{{url('/')}}#plan" @endif>@lang('Plan')</a></li>
-                    <li>
-                        <a href="{{route('faq')}}">@lang('Faq')</a>
-                    </li>
+                        <li><a @if(request()->path() == '/') href="#plan"
+                                @else href="{{url('/')}}#plan" @endif>@lang('Plan')</a></li>
+                        <li>
+                            <a href="{{route('faq')}}">@lang('Faq')</a>
+                        </li>
 
-                    <li>
-                        <a href="{{route('blog')}}">@lang('News')</a>
-                    </li>
+                        <li>
+                            <a href="{{route('blog')}}">@lang('News')</a>
+                        </li>
 
-                    <li>
-                        <a href="{{route('contact')}}">@lang('Contact')</a>
-                    </li>
+                        <li>
+                            <a href="{{route('contact')}}">@lang('Contact')</a>
+                        </li>
 
-                    <select id="langSel" class="select-bar">
+                        {{-- <select id="langSel" class="select-bar">
                         <option style="color: black" value="en">@lang('English')</option>
                         @foreach($lang as $data)
                             <option value="{{strtolower($data->code)}}"
-                                    @if(Session::get('lang') == strtolower($data->code)) selected="selected"
-                                    @endif style="color: black"> {{strtoupper($data->name)}}
-                            </option>
+                        @if(Session::get('lang') == strtolower($data->code)) selected="selected"
+                        @endif style="color: black"> {{strtoupper($data->name)}}
+                        </option>
                         @endforeach
-                    </select>
+                        </select> --}}
 
-                    <li>
-                        <a href="{{route('user.login')}}" class="header-button custom-button white">@lang('Sign In')</a>
-                    </li>
-                </ul>
-                <div class="header-bar d-lg-none">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <form class="search-form">
-                    <div class="form-group m-0">
-                        <input type="text" placeholder="Search Here">
-                        <button type="submit">
-                            <i class="flaticon-magnifying-glass"></i>
-                        </button>
+                        <li>
+                            <a href="{{route('user.login')}}"
+                                class="header-button custom-button white">@lang('Account')</a>
+                        </li>
+                    </ul>
+                    <div class="header-bar d-lg-none">
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
-                </form>
+                    <form class="search-form">
+                        <div class="form-group m-0">
+                            <input type="text" placeholder="Search Here">
+                            <button type="submit">
+                                <i class="flaticon-magnifying-glass"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
+        <div class="header-fix w-100"></div>
+
+    </header>
+
+
+
+    <div id="app">
+        @yield('content')
     </div>
-    <div class="header-fix w-100"></div>
-
-</header>
 
 
-
-  <div id="app">
-      @yield('content')
-  </div>
-
-
-<footer class="dark-bg bg_img" data-paroller-factor="0.5" data-paroller-type="background"
+    <footer class="dark-bg bg_img" data-paroller-factor="0.5" data-paroller-type="background"
         data-paroller-direction="vertical" data-background="./assets/images/shape/shape04.png">
-    <div class="footer-top padding-top padding-bottom">
-        <div class="container">
-            <div class="row mb-50-none justify-content-center">
-                <div class="col-sm-6 col-lg-8 ">
-                    <div class="footer-widget widget-about ">
-                        <div class="logo">
-                            <a href="{{url('/')}}"><img
+        <div class="footer-top padding-top padding-bottom">
+            <div class="container">
+                <div class="row mb-50-none justify-content-center">
+                    <div class="col-sm-6 col-lg-8 ">
+                        <div class="footer-widget widget-about ">
+                            <div class="logo">
+                                <a href="{{url('/')}}"><img
                                         src="{{get_image(config('constants.logoIcon.path') .'/logo.png')}}" alt="logo">
-                            </a>
-                        </div>
-                        <div class="content text-center">
-                            <p>{{__($footer->subtitle)}}</p>
-                            <ul class="social-icons-area">
-                                @foreach($social as $item)<li>
-                                            <a href="{{$item->value->url}}"
-                                               title="{{$item->value->title}}">@php echo $item->value->icon; @endphp</a></li>
-                                @endforeach
-                            </ul>
+                                </a>
+                            </div>
+                            <div class="content text-center">
+                                <p>{{__($footer->subtitle)}}</p>
+                                <ul class="social-icons-area">
+                                    @foreach($social as $item)<li>
+                                        <a href="{{$item->value->url}}" title="{{$item->value->title}}">@php echo
+                                            $item->value->icon; @endphp</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
-    <div class="footer-bottom text-center">
-        <div class="container">
-            <p class="m-0"> {{__($footer->title)}}</p>
+        <div class="footer-bottom text-center">
+            <div class="container">
+                <p class="m-0"> {{__($footer->title)}}</p>
+            </div>
         </div>
-    </div>
-    <div class="right banner-shape shape04"></div>
-</footer>
+        <div class="right banner-shape shape04"></div>
+    </footer>
 
-<script src="{{asset(activeTemplate(true) .'js/app.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/jquery-3.3.1.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/modernizr-3.6.0.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/plugins.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/bootstrap.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/isotope.pkgd.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/lightcase.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/swiper.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/wow.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/odometer.min.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/viewport.jquery.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/nice-select.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/paroller.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/js/main.js')}}"></script>
-
-
-<script src="{{asset(activeTemplate(true) .'front/js/iziToast.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'js/app.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/modernizr-3.6.0.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/plugins.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/lightcase.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/swiper.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/wow.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/odometer.min.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/viewport.jquery.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/nice-select.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/paroller.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/main.js')}}"></script>
 
 
-<script src="{{asset(activeTemplate(true) .'front/vue/vue-handle-error.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/vue/vue.js')}}"></script>
-<script src="{{asset(activeTemplate(true) .'front/vue/axios.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/js/iziToast.min.js')}}"></script>
 
 
-@include('partials.notify')
+    <script src="{{asset(activeTemplate(true) .'front/vue/vue-handle-error.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/vue/vue.js')}}"></script>
+    <script src="{{asset(activeTemplate(true) .'front/vue/axios.js')}}"></script>
 
-@yield('js')
 
-@stack('js')
+    @include('partials.notify')
 
-@yield('script')
+    @yield('js')
 
-<script>
-    $(document).on('change', '#langSel', function () {
+    @stack('js')
+
+    @yield('script')
+
+    <script>
+        $(document).on('change', '#langSel', function () {
         var code = $(this).val();
         window.location.href = "{{url('/')}}/change-lang/" + code;
     });
-</script>
+    </script>
 
 </body>
+
 </html>
-
-
